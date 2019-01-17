d81ffc370e886cd45ffd551610454e8cc6c45b12
qukang <qukang110@baidu.com>
2019-01-17 20:57:18 +0800

    test

--- agdu.sh
+++ agdu.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 comment=$1
-hexo g -d
-git add .
-git commit -m "$1"
-git push origin hexo
+if [ "$comment" != "" ];
+then
+ hexo g -d
+ git add .
+ git commit -m "$1"
+ git push origin hexo
+else
+ echo "comment cannot be null!"
+fi
